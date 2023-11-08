526d103ecf75944867a5d6c88e5a564c3ff6c31e
AbdulmoizAbdulmaged <149574102+AbdulmoizAbdulmaged@users.noreply.github.com>
2023-11-08 14:10:58 +0300

    first commit

---
+++ ZATCA QR/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZATCA_QR
+{
+    public partial class Form1 : Form
+    {
+       public List<string> lines = new List<string>();
+        public Image qrImage;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            textBox1.Text = "Aziz Bro";
+            textBox2.Text = "300526281800003";
+            textBox3.Text = "1.15";
+            textBox4.Text = "0.15";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+
+        {
+            string rn = "202310310198421.txt";
+            StringBuilder sb = new StringBuilder();
+            
+
+            string path = "C:\\Users\\Abdu\\OneDrive\\Desktop\\.NET\\C#\\ZATCA QR\\ZATCA QR\\" + rn;
+
+            lines = Zatca.readReceipt(path);
+
+            foreach(string txt in lines)
+            {
+                sb.Append(txt);
+                sb.Append("\n");
+               
+            }
+
+
+
+            richTextBox1.Text = sb.ToString();
+
+            
+        }
+
+        private void printZatcaQR_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+
+            Font f8 = new Font("Calibri", 11, FontStyle.Regular);
+            Font f9 = new Font("Calibri", 9, FontStyle.Bold);
+
+            int leftmargin = printZatcaQR.DefaultPageSettings.Margins.Left;
+            int centermagin = printZatcaQR.DefaultPageSettings.PaperSize.Width / 2;
+            int rightmargin = printZatcaQR.DefaultPageSettings.PaperSize.Width;
+
+            StringFormat right = new StringFormat();
+            StringFormat center = new StringFormat();
+            StringFormat left = new StringFormat();
+
+            int hight = 5;
+
+
+            right.Alignment = StringAlignment.Far;
+            center.Alignment = StringAlignment.Center;
+            left.Alignment = StringAlignment.Near;
+
+            foreach(String txt in lines)
+            {
+                e.Graphics.DrawString(txt, f9, Brushes.Black, leftmargin, hight, center);
+                hight = hight + 13;
+            }
+            e.Graphics.DrawImage(qrImage,Convert.ToInt32((e.PageBounds.Width - 170) / 2), hight + 11, 120, 120);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            qrImage = Zatca.generateQR(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text);
+
+            pictureBox1.Image = qrImage;
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+
+                printZatcaQR.Print();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ ZATCA QR/Zatca.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+using System.Drawing;
+using ZXing;
+using QRCoder;
+
+
+namespace ZATCA_QR
+{
+    class Zatca
+
+    {
+       
+        
+
+        public static List<string> readReceipt(String path)
+        {
+
+            List<string> list = new List<string>();
+            string receiptLine = "";
+
+            FileStream f = new FileStream(path, FileMode.OpenOrCreate);
+            StreamReader s = new StreamReader(f);
+            
+            using (s)
+            {
+                //receiptLine = s.ReadToEnd();
+
+                while (!s.EndOfStream)
+                {
+
+                    receiptLine = s.ReadLine();
+
+                    if (receiptLine.Length > 0)
+                        list.Add(receiptLine);
+                }
+                
+            }
+
+
+
+            return list;
+        }
+
+        public static Image generateQR(String aCompanyName,String aVATNumber,String aReceiptTotal,String aVATValue)
+        {
+            string value1 = getTLV(1, aCompanyName);
+            string value2 = getTLV(2, aVATNumber);
+            string value3 = getTLV(3, System.DateTime.Now.ToString());
+            string value4 = getTLV(4, aReceiptTotal);
+            string value5 = getTLV(5, aVATValue);
+
+            Byte[] b = System.Text.Encoding.UTF8.GetBytes(value1 + value2 + value3 + value4 + value5);
+            String t = Convert.ToBase64String(b);
+
+            QRCodeGenerator gen = new QRCodeGenerator();
+
+            QRCode qR = new QRCode(gen.CreateQrCode(t, QRCodeGenerator.ECCLevel.M));
+
+            Image qrImage;
+
+            qrImage = new Bitmap(qR.GetGraphic(6));
+
+            return qrImage;
+
+
+        }
+        public static String getTLV(int tag,String value)
+        {
+
+            string s = (char)tag + (char)value.Length + value;
+            return null;
+        }
+    }
+                
+
+           
+            
+
+
+
+            
+
+    
+}
